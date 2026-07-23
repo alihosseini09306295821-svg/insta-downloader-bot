f525bc780235df774c2cd52c0f9db1e3d86678c3
alihosseini09306295821-svg <alihosseini09306295821@gmail.com>
2026-07-23 14:00:59 +0330

    manager.py

--- downloaders/manager.py
+++ downloaders/manager.py
@@ -4,9 +4,9 @@ from .ytdlp import download_from_ytdlp
 
 async def download(url: str):
     methods = [
-        download_from_api,      # اول API
-        download_from_gallery,  # بعد gallery-dl
-        download_from_ytdlp,    # آخرین گزینه
+        download_from_api,
+        download_from_gallery,
+        download_from_ytdlp,
     ]
 
     last_error = None
