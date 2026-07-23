9f323b5eb739ed7fd246cf4eb1b3fa6aa179825e
alihosseini09306295821-svg <alihosseini09306295821@gmail.com>
2026-07-23 13:44:22 +0330

    ytdlp.py

--- downloaders/ytdlp.py
+++ downloaders/ytdlp.py
@@ -2,21 +2,21 @@ import os
 import tempfile
 import yt_dlp
 
-
 async def download_from_ytdlp(url: str):
     temp_dir = tempfile.mkdtemp()
-
+    
     ydl_opts = {
         "outtmpl": os.path.join(temp_dir, "%(title)s.%(ext)s"),
         "quiet": True,
         "noplaylist": True,
         "merge_output_format": "mp4",
+        "format": "bestvideo+bestaudio/best",  # کیفیت بهتر
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(url, download=True)
 
-        if "requested_downloads" in info:
+        if "requested_downloads" in info and info["requested_downloads"]:
             filename = info["requested_downloads"][0]["filepath"]
         else:
             filename = ydl.prepare_filename(info)
