83d1e0c5cb56a6ad4c595e01901b7f32486de753
alihosseini09306295821-svg <alihosseini09306295821@gmail.com>
2026-07-23 18:26:43 +0330

    Update users.py

--- database/users.py
+++ database/users.py
@@ -1,1 +1,43 @@
-# Coming Soon
+import sqlite3
+
+DB_NAME = "users.db"
+
+
+def init_db():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        user_id INTEGER PRIMARY KEY,
+        username TEXT,
+        first_name TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def add_user(user_id, username, first_name):
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("""
+    INSERT OR IGNORE INTO users(user_id, username, first_name)
+    VALUES(?,?,?)
+    """, (user_id, username, first_name))
+
+    conn.commit()
+    conn.close()
+
+
+def total_users():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+
+    cur.execute("SELECT COUNT(*) FROM users")
+    count = cur.fetchone()[0]
+
+    conn.close()
+    return count
