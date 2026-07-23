6a4a345396f97b95bf3be6a85a51ec09d678e69e
alihosseini09306295821-svg <alihosseini09306295821@gmail.com>
2026-07-23 15:02:16 +0330

    Add files via upload

---
+++ downloaders/manager.py
@@ -0,0 +1,20 @@
+from .api import download_from_api
+from .gallery import download_from_gallery
+from .ytdlp import download_from_ytdlp
+
+async def download(url: str):
+    methods = [
+        download_from_api,
+        download_from_gallery,
+        download_from_ytdlp,
+    ]
+
+    last_error = None
+
+    for method in methods:
+        try:
+            return await method(url)
+        except Exception as e:
+            last_error = e
+
+    raise Exception(f"All download methods failed: {last_error}")
