15a1437aad90d842c87178c59e45580978599a8e
alihosseini09306295821-svg <alihosseini09306295821@gmail.com>
2026-07-23 18:27:32 +0330

    Update join.py

--- handlers/join.py
+++ handlers/join.py
@@ -1,1 +1,35 @@
-# Coming Soon
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+CHANNEL_USERNAME = "@HmHermi"
+
+
+async def check_join(bot, user_id):
+    try:
+        member = await bot.get_chat_member(CHANNEL_USERNAME, user_id)
+
+        if member.status in ["member", "administrator", "creator"]:
+            return True
+
+    except:
+        pass
+
+    return False
+
+
+def join_keyboard():
+    keyboard = [
+        [
+            InlineKeyboardButton(
+                "📢 عضویت در کانال",
+                url="https://t.me/HmHermi"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "✅ عضو شدم",
+                callback_data="check_join"
+            )
+        ]
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
